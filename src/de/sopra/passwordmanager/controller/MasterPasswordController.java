d88957a0cffd36ac00deab879cb377a8866e7ea6
Arne Dußin <arne.dussin@live.de>
2019-08-21 18:50:04 +0200

    Add tests for MasterPasswordController

--- src/de/sopra/passwordmanager/controller/MasterPasswordController.java
+++ src/de/sopra/passwordmanager/controller/MasterPasswordController.java
@@ -2,46 +2,47 @@ package de.sopra.passwordmanager.controller;
 
 /**
  * Controller für Anforderungen der Masterpassworteingabe
- * @author Arne Dußin, Mel
  *
+ * @author Arne Dußin, Mel
  */
 public class MasterPasswordController {
-	
-	/**
-	 * Der GUI zugehörige PasswordManagerController 
-	 */
-	private PasswordManagerController passwordManagerController;
+    /**
+     * Der GUI zugehörige PasswordManagerController
+     */
+    private PasswordManagerController passwordManagerController;
 
-	public MasterPasswordController( PasswordManagerController controller )
-	{
-		this.passwordManagerController = controller;
-	}
+    public MasterPasswordController(PasswordManagerController controller) {
+        this.passwordManagerController = controller;
+    }
 
     /**
-	 * Das alte Masterpasswort wird mit dem neuen Masterpasswort überschrieben. 
-	 * Ein neuer Timer wird eingerichtet.
-	 * @param newPassword Passwort, mit dem der Datensatz von hier an (neu) verschlüsselt wird
-	 * @param newReminder Zeit in Tagen, in der der Nutzer zur Änderung des Masterpasswortes aufgefordert wird 
-	*/
-	public void changePassword(String newPassword, int newReminder) {
+     * Das alte Masterpasswort wird mit dem neuen Masterpasswort überschrieben.
+     * Ein neuer Timer wird eingerichtet.
+     *
+     * @param newPassword Passwort, mit dem der Datensatz von hier an (neu) verschlüsselt wird
+     * @param newReminder Zeit in Tagen, in der der Nutzer zur Änderung des Masterpasswortes aufgefordert wird
+     */
+    public void changePassword(String newPassword, int newReminder) {
 
-	}
-	
-	/**
-	 * Überprüft, ob das übergebene Passwort mit dem momentanen Masterpasswort übereinstimmt.
-	 * @param password Das zu überprüfende Passwort
-	 * @return true, wenn das übergebene Passwort mit dem hinterlegten Masterpasswort übereinstimmt. false, sonst.
-	 */
-	boolean checkPassword(String password) {
-		return false;
-	}
-	
-	/**
-	 * Überprüft, ob der Timer des momentanen Masterpasswortes abgelaufen ist. 
-	 * @return true, wenn Masterpasswort geändert werden muss. false, sonst.
-	 */
-	boolean hasToBeChanged() {
-		return false;
-	}
+    }
+
+    /**
+     * Überprüft, ob das übergebene Passwort mit dem momentanen Masterpasswort übereinstimmt.
+     *
+     * @param password Das zu überprüfende Passwort
+     * @return true, wenn das übergebene Passwort mit dem hinterlegten Masterpasswort übereinstimmt. false, sonst.
+     */
+    boolean checkPassword(String password) {
+        return false;
+    }
+
+    /**
+     * Überprüft, ob der Timer des momentanen Masterpasswortes abgelaufen ist.
+     *
+     * @return true, wenn Masterpasswort geändert werden muss. false, sonst.
+     */
+    boolean hasToBeChanged() {
+        return false;
+    }
 
 }
---
+++ tests/de/sopra/passwordmanager/controller/MasterPasswordControllerTest.java
@@ -0,0 +1,26 @@
+package de.sopra.passwordmanager.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MasterPasswordControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void changePassword() {
+    }
+
+    @Test
+    public void checkPassword() {
+    }
+
+    @Test
+    public void hasToBeChanged() {
+
+    }
+}
