72f5bbfb9fb0da96e130b71cb0a443f5add8fe8c
Arne Dußin <arne.dussin@live.de>
2019-08-21 17:18:23 +0200

    Add docs for PasswordManager

--- src/de/sopra/passwordmanager/model/PasswordManager.java
+++ src/de/sopra/passwordmanager/model/PasswordManager.java
@@ -1,25 +1,23 @@
 package de.sopra.passwordmanager.model;
 
-public class PasswordManager
-{
-
+/**
+ * Enthält alle Daten die für den PasswortManager relevant sind in verschlüsseltem Zustand.
+ */
+public class PasswordManager {
+    /**
+     * Passwort mit dem die Daten im PasswordManager momentan verschlüsselt sind. Es ist das einzige Passwort, was
+     * unverschlüsselt im Speicher liegt und nicht mit im Datensatz gespeichert wird.
+     */
     private BasePassword masterPassword;
 
+    /**
+     * Die Hauptkategorie, in der alle Unterkategorien des Datenmodells enthalten sind.
+     */
     private Category rootCategory;
 
-    public BasePassword getMasterPassword()
-    {
-        return masterPassword;
-    }
+    public BasePassword getMasterPassword() { return masterPassword; }
 
-    public Category getRootCategory()
-    {
-        return rootCategory;
-    }
+    public Category getRootCategory() { return rootCategory; }
 
-    public void setMasterPassword( BasePassword masterPassword )
-    {
-        this.masterPassword = masterPassword;
-    }
-
+    public void setMasterPassword( BasePassword masterPassword ) { this.masterPassword = masterPassword; }
 }
