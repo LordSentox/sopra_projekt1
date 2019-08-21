a479457014feffcd91c1da430cb83e43c17b1854
Juyas <juyasetgames@gmail.com>
2019-08-21 11:38:12 +0200

    + added CategoryTest, in order to recreate the test folder. the missing test folder leads to errors in eclipse

---
+++ tests/de/sopra/passwordmanager/model/CategoryTest.java
@@ -0,0 +1,13 @@
+package de.sopra.passwordmanager.model;
+
+/**
+ * <h1>projekt1</h1>
+ *
+ * @author Julius Korweck
+ * @version 21.08.2019
+ * @since 21.08.2019
+ */
+public class CategoryTest
+{
+
+}
