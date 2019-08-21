271c52bc350c4624baff3bcae7fafdecccd1ecba
sopr046 <sopr046@fiws216.cs.tu-dortmund.de>
2019-08-21 17:10:27 +0200

    Added most TestCases for CategoryController, edited Doc

--- src/de/sopra/passwordmanager/controller/CategoryController.java
+++ src/de/sopra/passwordmanager/controller/CategoryController.java
@@ -5,19 +5,17 @@ import de.sopra.passwordmanager.model.Category;
 import java.util.ArrayList;
 
 /**
- * @author sopr046
- * @author sopr042
+ * sorgt für das Erstellen, Bearbeiten und Löschen. Intern kann nach Kategorien gesucht werden.
+ * @authors Max, Lisa
  *
  */
 public class CategoryController {
 
-	/**
-	 * 
-	 */
+
 	private PasswordManagerController passwordManagerController;
 
 	/**
-	 * createCategory erstellt eine neue Kategorie
+	 * erstellt eine neue Kategorie
 	 * die eine Oberkategorie und einen Namen haben muss.
 	 * Die anzulegende Kategorie muss der Oberkategorie als Kind angehängt werden.
 	 * 
@@ -36,8 +34,8 @@ public class CategoryController {
 	}
 
 	/**
-	 * removeCategory entfernt die Kategorie aus dem Datenmodell.
-	 * 				  Je nach übergebenem Boolean werden die enthaltenen Credentials und Unterkategorien mit gelöscht oder nicht.
+	 * entfernt die Kategorie aus dem Datenmodell.
+	 * Je nach übergebenem Boolean werden die enthaltenen Credentials und Unterkategorien mit gelöscht oder nicht.
 	 * @param category Die zu löschende Kategorie, darf nicht Null sein 
 	 * 			   	   Wenn das der Fall ist, wird die MainWindowAUI geholt und darauf showError("Es muss eine Kategorie ausgewählt sein") aufgerufen
 	 * @param removeCredentialsToo gibt an, ob die enthaltenen Credentials und Unterkategorien mit gelöscht werden oder nicht.
@@ -56,11 +54,11 @@ public class CategoryController {
 	}
 
 	/**
-	 * moveCategory holt sich mittels getCategory(oldPath) die zu verschiebende Kategorie [oldPath mit dem Namen der zu verschiebenden Kategorie] 
-	 * 			    und die zugehörige Oberkategorie [oldPath ohne den Namen der zu verschiebenden Kategorie],
-	 * 				mittels getCategory(newPath) die neue Oberkategorie [newPath ohne den Namen der zu verschiebenden Kategorie] und hängen dieser die zu verschiebende Kategorie an. 
-	 * 				Danach wird die Referenz von der Alten Oberkategorie auf die verschobene Kategorie entfernt.
-	 * 				Dann wird aus newPath der neue Name für die verschobene Kategorie ausgelesen und gesetzt.
+	 * holt sich mittels getCategory(oldPath) die zu verschiebende Kategorie [oldPath mit dem Namen der zu verschiebenden Kategorie] 
+	 * und die zugehörige Oberkategorie [oldPath ohne den Namen der zu verschiebenden Kategorie],
+	 * mittels getCategory(newPath) die neue Oberkategorie [newPath ohne den Namen der zu verschiebenden Kategorie] und hängen dieser die zu verschiebende Kategorie an. 
+	 * Danach wird die Referenz von der Alten Oberkategorie auf die verschobene Kategorie entfernt.
+	 * Dann wird aus newPath der neue Name für die verschobene Kategorie ausgelesen und gesetzt.
 	 * @param oldPath kompletter Pfad bis inkl. Kategoriename der zu verschiebenden Kategorie
 	 * @param newPath kompletter Pfad bis inkl. Kategoriename der verschobenen Kategorie
 	 * @throws IllegalArgumentException wenn einer der Pfade null ist, wird die Exception geworfen
@@ -70,10 +68,10 @@ public class CategoryController {
 	}
 
 	/**
-	 * getCategory holt sich vom passwordManagerController die rootCategory. 
-	 * 			   Dann wird der übergebene Pfad Schritt für Schritt (Kategorien getrennt durch "/") durchlaufen 
-	 * 			   und jeweils die entsprechende Unterkategorie aus dem Datenmodell geholt, bis der String abgearbeitet ist.
-	 * 			   Die letzte geholte Kategorie ist die gesuchte, die zurückgegeben wird.
+	 * holt sich vom passwordManagerController die rootCategory. 
+	 * Dann wird der übergebene Pfad Schritt für Schritt (Kategorien getrennt durch "/") durchlaufen 
+	 * und jeweils die entsprechende Unterkategorie aus dem Datenmodell geholt, bis der String abgearbeitet ist.
+	 * Die letzte geholte Kategorie ist die gesuchte, die zurückgegeben wird.
 	 * @param path kompletter Pfad bis inkl. Kategoriename der gesuchten Kategorie.
 	 * 			   wenn der Pfad null oder leerer String ist, wird die rootCategory zurükgegeben.
 	 * @return gibt die letzte Kategorie des angegebenen Pfades zurück
@@ -83,10 +81,10 @@ public class CategoryController {
 	}
 
 	/**
-	 * findCategory holt sich mittels getCategory() die rootCategory und erstellt eine leere ArrayList<Category>.
-	 * 			    Dann durchsucht es alle Unterkategorien und fügt alle, deren Name dem eingegebenen Namen entspricht der ArrayList<Category> hinzu.
-	 * 				Sind alle Unterkategorien durchsucht, wird die ArrayList<Category> zurückgegeben.
-	 * 				Gibt es keine passende Unterkategorie, ist die ArrayList<Category> leer und wird ebenfalls zurückgegeben. 
+	 * holt sich mittels getCategory() die rootCategory und erstellt eine leere ArrayList<Category>.
+	 * Dann durchsucht es alle Unterkategorien und fügt alle, deren Name dem eingegebenen Namen entspricht der ArrayList<Category> hinzu.
+	 * Sind alle Unterkategorien durchsucht, wird die ArrayList<Category> zurückgegeben.
+	 * Gibt es keine passende Unterkategorie, ist die ArrayList<Category> leer und wird ebenfalls zurückgegeben. 
 	 * @param name der Name der gesuchten Unterkategorien
 	 * @return die angelegte und (eventuell) gefüllte ArrayList<Category>
 	 */
