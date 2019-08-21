d0d84dc98d9f24b46930303428b40ccfb149203e
Arne Dußin <arne.dussin@live.de>
2019-08-21 16:04:59 +0200

    Revert "Add docs for BasePassword"
    This reverts commit 9044e5ad10a4bad901c55580b1bfa8753e8f65bb.

--- src/de/sopra/passwordmanager/model/BasePassword.java
+++ src/de/sopra/passwordmanager/model/BasePassword.java
@@ -1,56 +1,48 @@
 package de.sopra.passwordmanager.model;
 
-/**
- * Die einfachsten Passwort-Funktionalitäten. Sie werden sowohl von dem Masterpasswort, als auch vom Passwortteil in den
- * Credentials gebraucht.
- *
- * @author Arne Dußin
- */
-public class BasePassword {
-    /**
-     * Der tatsächliche Passwortstring. Er liegt nur verschlüsselt im Speicher, außer beim Masterpasswort. In diesem Fall
-     * ist es das tatsächliche Passwort.
-     */
+public class BasePassword
+{
+
     private String password;
 
-    /**
-     * Der Zeitpunkt, zu dem das Passwort das letzte Mal geändert wurde, gespeichert als lokaler Unix-Zeitstempel.
-     */
     private long lastChanged;
 
-    /**
-     * Die Zeit in Tagen, nach denen ein Passwort geändert werden soll. Kann mit lastChanged verglichen werden
-     * <code>null</code>, falls nicht erinnert werden soll, also das Passwort ewig verwendet werden kann.
-     */
     private Integer changeReminderDays;
 
-    public BasePassword(String password, long lastChanged, Integer changeReminderDays) {
+    public BasePassword( String password, long lastChanged, Integer changeReminderDays )
+    {
         this.password = password;
         this.lastChanged = lastChanged;
         this.changeReminderDays = changeReminderDays;
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
-    public Integer getChangeReminderDays() {
+    public Integer getChangeReminderDays()
+    {
         return changeReminderDays;
     }
 
-    public long getLastChanged() {
+    public long getLastChanged()
+    {
         return lastChanged;
     }
 
-    public void setPassword(String password) {
+    public void setPassword( String password )
+    {
         this.password = password;
     }
 
-    public void setChangeReminderDays(Integer changeReminderDays) {
+    public void setChangeReminderDays( Integer changeReminderDays )
+    {
         this.changeReminderDays = changeReminderDays;
     }
 
-    public void setLastChanged(long lastChanged) {
+    public void setLastChanged( long lastChanged )
+    {
         this.lastChanged = lastChanged;
     }
 
