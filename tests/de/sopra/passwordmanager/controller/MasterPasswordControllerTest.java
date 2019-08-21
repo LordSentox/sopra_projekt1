32b0ed3a306e75415f6e77f0f56fe6e1ffd031cc
Arne Dussin <arne.dussin@live.de>
2019-08-21 21:05:37 +0200

    Add helper function to calculate milliseconds from days

--- tests/de/sopra/passwordmanager/controller/MasterPasswordControllerTest.java
+++ tests/de/sopra/passwordmanager/controller/MasterPasswordControllerTest.java
@@ -12,8 +12,12 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 public class MasterPasswordControllerTest {
-    PasswordManagerController passwordManagerController;
-    PasswordManager passwordManager;
+    private PasswordManagerController passwordManagerController;
+    private PasswordManager passwordManager;
+
+    private long daysToMillis(int days) {
+        return days * 3600 * 24 * 1000;
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -34,19 +38,19 @@ public class MasterPasswordControllerTest {
     public void hasToBeChangedTest() {
         // Setze das Masterpasswort auf ein im Fünftagetakt zu Änderndes und teste, ob die Methode richtig erkennt, ob ein
         // dass ein vor sechs Tagen geändertes Passwort geändert werden muss
-        Date sixDaysEarlier = new Date(System.currentTimeMillis() - 3600*24*6*1000);
+        Date sixDaysEarlier = new Date(System.currentTimeMillis() - daysToMillis(6));
         this.passwordManager.setMasterPassword(new BasePassword("hello_there", 5, sixDaysEarlier));
         Assert.assertTrue(this.passwordManagerController.getMasterPasswordController().hasToBeChanged());
 
         // Setze das Masterpasswort auf ein Passwort, welches nicht geändert werden muss, und teste, ob es als noch
         // gültiges Passwort erkannt werden muss
-        sixDaysEarlier = new Date(System.currentTimeMillis() - 3600*24*6*1000);
-        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 7, sixDaysEarlier));
+        Date fourDaysEarlier = new Date(System.currentTimeMillis() - daysToMillis(4));
+        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 7, fourDaysEarlier));
         Assert.assertFalse(this.passwordManagerController.getMasterPasswordController().hasToBeChanged());
 
         // Setze das Masterpasswort auf ein Datum, welches gerade heute geändert werden muss.
         // Auch hier muss es geändert werden.
-        Date halfYearEarlier = new Date(System.currentTimeMillis() - 3600*24*182*1000);
+        Date halfYearEarlier = new Date(System.currentTimeMillis() - daysToMillis(182));
         this.passwordManager.setMasterPassword(new BasePassword("hello_there", 182, halfYearEarlier));
         Assert.assertTrue(this.passwordManagerController.getMasterPasswordController().hasToBeChanged());
     }
