754a35991756b879bf781f3ea2e949879a9919a4
sopr049 <sopr049@fiws219.cs.tu-dortmund.de>
2019-08-21 15:49:35 +0200

    created UtilityControllerTest and corrected docs in UtilityController

--- src/de/sopra/passwordmanager/controller/UtilityController.java
+++ src/de/sopra/passwordmanager/controller/UtilityController.java
@@ -3,7 +3,7 @@ package de.sopra.passwordmanager.controller;
 import java.io.File;
 
 /**
- * Der UtilityController stellt verschiedene Hilfsdienste zur Verfuegung
+ * Der UtilityController stellt verschiedene Hilfsdienste zur Verfügung
  * @author sopr049, sopr043
  *
  */
@@ -15,52 +15,53 @@ public class UtilityController {
 	private PasswordManagerController passwordManagerController;
 	
 	/**
-	 * Generiert ein Passwort, welches den Sicherheitsanforderungen entspricht
+	 * Generiert ein Passwort, welches den Sicherheitsanforderungen entspricht nd dieses wird dann in der GUI angezeigt
 	 */
 	public void generatePassword() {
 
 	}
 	
 	/**
-	 * Die Methode exportiert die aktuellen Daten in die angegebene Datei
+	 * Die Methode exportiert die aktüllen Daten in die angegebene Datei, wenn die Datei bereits etwas enthält, wird diese überschrieben
 	 * @param file Die Datei, in welche die daten exportiert werden sollen
+	 * @throws IllegalArgumentException Wenn file null ist oder der Pfad nicht existiert
 	 */
-	public void exportFile(File file) {
+	public void exportFile(File file) throws IllegalArgumentException {
 
 	}
 	
 	/**
-	 * Die Methode entschluesselt einen eingegebenen text mit dem Masterpasswort
-	 * @param text Der zu entschluesselnde Text, dabei kann es sich um Passwoerter oder die Sicherheitsfragen handeln
-	 * @return Der zurueckgegebene String ist die entschluesselte Version des eingegebenen Textes 
+	 * Die Methode entschlüsselt einen eingegebenen text mit dem Masterpasswort
+	 * @param text Der zu entschlüsselnde Text, dabei kann es sich um Passwörter oder die Sicherheitsfragen handeln
+	 * @return Der zurückgegebene String ist die entschlüsselte Version des eingegebenen Textes 
 	 */
 	String encryptText(String text) {
 		return null;
 	}
 	
 	/**
-	 * Die Methode verschluesselt einen eingegebenen text mit dem Masterpasswort
-	 * @param text Der zu verschluesselnde Text, dabei kann es sich um Passwoerter oder die Sicherheitsfragen handeln
-	 * @return Der zurueckgegebene String ist die verschluesselte Version des eingegebenen Textes 
+	 * Die Methode verschlüsselt einen eingegebenen text mit dem Masterpasswort
+	 * @param text Der zu verschlüsselnde Text, dabei kann es sich um Passwörter oder die Sicherheitsfragen handeln
+	 * @return Der zurückgegebene String ist die verschlüsselte Version des eingegebenen Textes 
 	 */
 	String decryptText(String text) {
 		return null;
 	}
 	/**
-	 * Diese Methode ueberprueft die Qualitaet eines Passwortes und gibt eine Zahl zwischen 0 und 100 zurueck ,wobei mehr besser ist
-	 * @param text Das zu ueberpruefende Passwort
-	 * @return Es wird ein Wert von 0 bis 100 geliefert, der die Qualitaet des Passwortes angibt, dabei steht 0 fuer sehr schlecht und 100 fuer sehr sicher
+	 * Diese Methode überprüft die Qualität eines Passwortes und gibt eine Zahl zwischen 0 und 100 zurück ,wobei mehr besser ist
+	 * @param text Das zu überprüfende Passwort
+	 * @return Es wird ein Wert von 0 bis 100 geliefert, der die Qualität des Passwortes angibt, dabei steht 0 für sehr schlecht und 100 für sehr sicher
 	 */
 	int checkQuality(String text) {
 		return 0;
 	}
 	
 	/**
-	 * Die Methode importiert eine neue Datei mit Anmeldedaten. Fuer den Import wird das Masterpasswort der Datei benoetigt.
-	 * Das Importieren einer neuen Datei ueberschreibt die aktuellen Eintraege.
+	 * Die Methode importiert eine neü Datei mit Anmeldedaten. Für den Import wird das Masterpasswort der Datei benötigt.
+	 * Das Importieren einer neün Datei überschreibt die aktüllen Einträge.
 	 * @param file Die zu importierende Datei
 	 * @param masterPassword das Masterpasswort des zu importierenden Projektes
-	 * @return Die Methodee liefert false, wenn ein fehler beim importieren passiert, wenn true geliefert wird, 
+	 * @return Die Methode liefert false, wenn ein fehler beim importieren passiert, wenn true geliefert wird, 
 	 * 		   hat der Import funktioniert
 	 */
 	boolean importFile(File file, String masterPassword) {
