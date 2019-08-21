cffe87b7cafd60aad5edb9ad35765b5e8bf7f89a
Juyas <juyasetgames@gmail.com>
2019-08-21 15:04:00 +0200

    + added empty test dummy for testing

---
+++ tests/de/sopra/passwordmanager/controller/PasswordManagerControllerDummy.java
@@ -0,0 +1,24 @@
+package de.sopra.passwordmanager.controller;
+
+/**
+ * <h1>projekt1</h1>
+ *
+ * @author Julius Korweck
+ * @version 21.08.2019
+ * @since 21.08.2019
+ */
+public class PasswordManagerControllerDummy
+{
+
+    public static PasswordManagerController getNewController()
+    {
+        //TODO: create new dummy controller
+        return null;
+    }
+
+}
+/***********************************************************************************************
+ *
+ *                  All rights reserved, SpaceParrots UG (c) copyright 2019
+ *
+ ***********************************************************************************************/
