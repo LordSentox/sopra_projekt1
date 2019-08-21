ecabbbd740a3fd6d3109d5ae89df4b9a138532a5
Juyas <juyasetgames@gmail.com>
2019-08-21 18:49:38 +0200

    + added some useful methods to create or remove entries or subcategories in Category + added a method to get a category by its relative path starting by the current category

--- src/de/sopra/passwordmanager/model/Category.java
+++ src/de/sopra/passwordmanager/model/Category.java
@@ -1,7 +1,10 @@
 package de.sopra.passwordmanager.model;
 
+import de.sopra.passwordmanager.util.Path;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 public class Category
 {
@@ -12,8 +15,7 @@ public class Category
 
     private Collection<Category> subCategories;
 
-    public Category( String name )
-    {
+    public Category( String name ) {
         this.name = name;
         this.credentials = new ArrayList<>();
         this.subCategories = new ArrayList<>();
@@ -32,6 +34,43 @@ public class Category
     public Collection<Category> getSubCategories()
     {
         return subCategories;
+    }
+
+    public void addSubCategory( Category category ) {
+        subCategories.add( category );
+    }
+
+    public void removeSubCategory( Category category ) {
+        subCategories.remove( category );
+    }
+
+    public void addCredentials(Credentials credentials) {
+        this.credentials.add( credentials );
+    }
+
+    public void removeCredentials(Credentials credentials) {
+        this.credentials.remove( credentials );
+    }
+
+    public void removeCredentialsFromTree(Credentials credentials) {
+        removeCredentials( credentials );
+        subCategories.forEach( s -> s.removeCredentialsFromTree( credentials ) );
+    }
+
+    public Category getCategoryByPath(Path path) {
+        path.navigate( 0 );
+        if(path.getName().equals( getName() )) {
+            if(path.length() == 1) {
+                return this;
+            } else {
+                Optional<Category> any = subCategories.stream()
+                        .filter( s -> s.getCategoryByPath( path.subPath( 1, path.length() ) ) != null )
+                        .findAny();
+                if(any.isPresent()) return any.get();
+                else return null;
+            }
+        }
+        else return null;
     }
 
 }
