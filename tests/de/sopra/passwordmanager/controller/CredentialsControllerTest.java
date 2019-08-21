e335cfed2b16f62cd7d04c58e2fc1781b2bf67fd
skadic <etienne.palanga@tu-dortmund.de>
2019-08-21 22:46:17 +0200

    + Added tests for CredentialsBuilder + Added some tests for CredentialsController

--- tests/de/sopra/passwordmanager/controller/CredentialsControllerTest.java
+++ tests/de/sopra/passwordmanager/controller/CredentialsControllerTest.java
@@ -1,5 +1,9 @@
 package de.sopra.passwordmanager.controller;
 
+import de.sopra.passwordmanager.model.Credentials;
+import de.sopra.passwordmanager.model.PasswordManager;
+import de.sopra.passwordmanager.util.CredentialsBuilder;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,17 +16,90 @@ import org.junit.Test;
  */
 public class CredentialsControllerTest {
 
+    private PasswordManagerController pmc;
+    private CredentialsController cc;
+    private PasswordManager pm;
+
     @Before
     public void setUp() throws Exception {
+        pmc = PasswordManagerControllerDummy.getNewController();
+        cc = pmc.getCredentialsController();
+        pm = pmc.getPasswordManager();
     }
 
     @Test
-    public void saveCredentials() {
+    public void saveCredentialsBasic() {
+        Credentials cred = new CredentialsBuilder()
+                .withName("cred1")
+                .withUserName("user1")
+                .withPassword("passwort123")
+                .withWebsite("www.hallo.de")
+                .build();
 
+        cc.saveCredentials(null, cred);
+        Assert.assertTrue(pmc.getPasswordManager().getRootCategory().getCredentials().contains(cred));
+    }
+
+    @Test
+    public void replaceCredentialsBasic() {
+        Credentials cred1 = new CredentialsBuilder()
+                .withName("cred1")
+                .withUserName("user1")
+                .withPassword("passwort123")
+                .withWebsite("www.hallo.de")
+                .build();
+
+        Credentials cred2 = new CredentialsBuilder()
+                .withName("cred2")
+                .withUserName("user2")
+                .withPassword("456passwort")
+                .withWebsite("www.hi.com")
+                .build();
+
+        cc.saveCredentials(null, cred1);
+        Assert.assertTrue(pm.getRootCategory().getCredentials().contains(cred1));
+        cc.saveCredentials(cred1, cred2);
+
+        //Nach dem ersetzen darf nur 1 Eintrag im Passwortmanager existieren und dieser muss mit cred2 übereinstimmen
+        Assert.assertEquals(1, pm.getRootCategory().getCredentials().size());
+        Assert.assertTrue(pm.getRootCategory().getCredentials().contains(cred2));
+    }
+
+    @Test
+    public void replaceCredentialsSameObject() {
+        Credentials cred = new CredentialsBuilder()
+                .withName("cred")
+                .withUserName("user1")
+                .withPassword("passwort123")
+                .withWebsite("www.hallo.de")
+                .build();
+        cc.saveCredentials(null, cred);
+        Assert.assertTrue(pm.getRootCategory().getCredentials().contains(cred));
+        cc.saveCredentials(cred, cred);
+
+        //Nach dem ersetzen darf nur 1 Eintrag im Passwortmanager existieren und dieser muss mit cred übereinstimmen
+        Assert.assertEquals(1, pm.getRootCategory().getCredentials().size());
+        Assert.assertTrue(pm.getRootCategory().getCredentials().contains(cred));
+    }
+
+    @Test
+    public void saveCredentialsNull() {
+        Credentials cred = new CredentialsBuilder()
+                .withName("cred1")
+                .withUserName("user1")
+                .withPassword("passwort123")
+                .withWebsite("www.hallo.de")
+                .build();
+
+        cc.saveCredentials(cred, null);
+        Assert.assertEquals(0, pm.getRootCategory().getCredentials().size());
+        cc.saveCredentials(null, null);
+        Assert.assertEquals(0, pm.getRootCategory().getCredentials().size());
     }
 
     @Test
     public void removeCredentials() {
+
     }
 
     @Test
