ea97a58e15ca6e5a54b28b3eb2b4014b5819e7b5
sopr049 <sopr049@fiws216.cs.tu-dortmund.de>
2019-08-22 11:56:05 +0200

    Create BasePasswordTest

--- tests/de/sopra/passwordmanager/model/BasePasswordTest.java
+++ tests/de/sopra/passwordmanager/model/BasePasswordTest.java
@@ -6,11 +6,12 @@ import org.junit.Assert;
 
 import java.util.Date;
 
-public class BasePasswordTest{
+public class BasePasswordTest {
+
 	@Test
-	public void getSetTest(){ 
-		Date now= new Date();
-		BasePassword bp= new BasePassword("pw",1,now);
+	public void getSetTest() {
+		Date now = new Date();
+		BasePassword bp = new BasePassword("pw",1,now);
 		bp.setChangeReminderDays(2);
 		Assert.assertEquals("retrieved value not equal to expected",new Integer(2), bp.getChangeReminderDays());
 		Date now2= new Date();
