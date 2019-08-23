7fe3f95416c2932932db3997c9f036e60bdeb336
Juyas <juyasetgames@gmail.com>
2019-08-23 16:02:06 +0200

    Add EncryptedString

---
+++ src/de/sopra/passwordmanager/util/EncryptedString.java
@@ -0,0 +1,27 @@
+package de.sopra.passwordmanager.util;
+
+/**
+ * <h1>projekt1</h1>
+ *
+ * @author Julius Korweck
+ * @version 23.08.2019
+ * @since 23.08.2019
+ */
+public final class EncryptedString {
+
+    private final String encryptedContent;
+
+    public EncryptedString(String encryptedContent) {
+        this.encryptedContent = encryptedContent;
+    }
+
+    public String getEncryptedContent() {
+        return encryptedContent;
+    }
+
+}
+/***********************************************************************************************
+ *
+ *                  All rights reserved, SpaceParrots UG (c) copyright 2019
+ *
+ ***********************************************************************************************/
