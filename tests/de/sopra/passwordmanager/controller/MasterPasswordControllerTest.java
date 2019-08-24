0d40ee7f2eeda317e244fd1fb5dd47d7c8d5451c
Juyas <juyasetgames@gmail.com>
2019-08-24 14:29:26 +0200

    Fix MasterPasswordControllerTest according to the removal of BasePassword Fix UtilityControllerTest according to the removal of BasePassword

--- tests/de/sopra/passwordmanager/controller/MasterPasswordControllerTest.java
+++ tests/de/sopra/passwordmanager/controller/MasterPasswordControllerTest.java
@@ -1,6 +1,5 @@
 package de.sopra.passwordmanager.controller;
 
-import de.sopra.passwordmanager.model.BasePassword;
 import de.sopra.passwordmanager.model.PasswordManager;
 import org.junit.Assert;
 import org.junit.Before;
@@ -25,7 +24,9 @@ public class MasterPasswordControllerTest {
         //assertion, wenn das alte Passwort weiterhin gespeichert ist
         //assertion, wenn neues Passwort nicht gespeichert wurde
         LocalDateTime sixDaysEarlier = LocalDateTime.now().minus(6, ChronoUnit.DAYS);
-        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 5, sixDaysEarlier));
+        this.passwordManager.setMasterPassword("hello_there");
+        this.passwordManager.setMasterPasswordReminderDays(5);
+        this.passwordManager.setMasterPassordLastChanged(sixDaysEarlier);
         this.passwordManagerController.getMasterPasswordController().changePassword("hello", 10);
 
 
@@ -40,7 +41,9 @@ public class MasterPasswordControllerTest {
         //prüft, ob true ausgegeben wird, wenn dieses Passwort eingegeben wird
         //prüft, ob false ausgegeben wird, wenn dieses Passwort nicht eingegeben wird
         LocalDateTime sixDaysEarlier = LocalDateTime.now().minus(6, ChronoUnit.DAYS);
-        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 5, sixDaysEarlier));
+        this.passwordManager.setMasterPassword("hello_there");
+        this.passwordManager.setMasterPasswordReminderDays(5);
+        this.passwordManager.setMasterPassordLastChanged(sixDaysEarlier);
         Assert.assertTrue(this.passwordManagerController.getMasterPasswordController().checkPassword("hello_there"));
 
         Assert.assertFalse(this.passwordManagerController.getMasterPasswordController().checkPassword("hello_tree"));
@@ -51,19 +54,25 @@ public class MasterPasswordControllerTest {
         // Setze das Masterpasswort auf ein im Fünftagetakt zu Änderndes und teste, ob die Methode richtig erkennt, ob ein
         // dass ein vor sechs Tagen geändertes Passwort geändert werden muss
         LocalDateTime sixDaysEarlier = LocalDateTime.now().minus(6, ChronoUnit.DAYS);
-        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 5, sixDaysEarlier));
+        this.passwordManager.setMasterPassword("hello_there");
+        this.passwordManager.setMasterPasswordReminderDays(5);
+        this.passwordManager.setMasterPassordLastChanged(sixDaysEarlier);
         Assert.assertTrue(this.passwordManagerController.getMasterPasswordController().hasToBeChanged());
 
         // Setze das Masterpasswort auf ein Passwort, welches nicht geändert werden muss, und teste, ob es als noch
         // gültiges Passwort erkannt werden muss
         LocalDateTime fourDaysEarlier = LocalDateTime.now().minus(4, ChronoUnit.DAYS);
-        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 7, fourDaysEarlier));
+        this.passwordManager.setMasterPassword("hello_there");
+        this.passwordManager.setMasterPasswordReminderDays(7);
+        this.passwordManager.setMasterPassordLastChanged(fourDaysEarlier);
         Assert.assertFalse(this.passwordManagerController.getMasterPasswordController().hasToBeChanged());
 
         // Setze das Masterpasswort auf ein Datum, welches gerade heute geändert werden muss.
         // Auch hier muss es geändert werden.
         LocalDateTime halfYearEarlier = LocalDateTime.now().minus(182, ChronoUnit.DAYS);
-        this.passwordManager.setMasterPassword(new BasePassword("hello_there", 182, halfYearEarlier));
+        this.passwordManager.setMasterPassword("hello_there");
+        this.passwordManager.setMasterPasswordReminderDays(182);
+        this.passwordManager.setMasterPassordLastChanged(halfYearEarlier);
         Assert.assertTrue(this.passwordManagerController.getMasterPasswordController().hasToBeChanged());
     }
 }
--- tests/de/sopra/passwordmanager/controller/UtilityControllerTest.java
+++ tests/de/sopra/passwordmanager/controller/UtilityControllerTest.java
@@ -1,10 +1,10 @@
 package de.sopra.passwordmanager.controller;
 
 import de.sopra.passwordmanager.controller.PasswordManagerControllerDummy.MainView;
-import de.sopra.passwordmanager.model.BasePassword;
 import de.sopra.passwordmanager.model.Category;
 import de.sopra.passwordmanager.model.Credentials;
 import de.sopra.passwordmanager.util.CredentialsBuilder;
+import de.sopra.passwordmanager.util.EncryptedString;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,93 +17,97 @@ import java.time.LocalDateTime;
  */
 
 public class UtilityControllerTest {
-	private PasswordManagerController passwordManagerController;
-	private UtilityController utilityController;
-	private MainView mainWindowAUI;
-	
+    private PasswordManagerController passwordManagerController;
+    private UtilityController utilityController;
+    private MainView mainWindowAUI;
+
     @Before
     public void setUp() throws Exception {
-    	passwordManagerController = PasswordManagerControllerDummy.getNewController();
-    	utilityController = passwordManagerController.getUtilityController();
-    	mainWindowAUI = (MainView) passwordManagerController.getMainWindowAUI();
+        passwordManagerController = PasswordManagerControllerDummy.getNewController();
+        utilityController = passwordManagerController.getUtilityController();
+        mainWindowAUI = (MainView) passwordManagerController.getMainWindowAUI();
     }
 
     @Test
     public void generatePasswordTest() {
-    	utilityController.generatePassword();
-    	String password = mainWindowAUI.getPasswordShown();
-    	
-    	Assert.assertTrue(utilityController.checkQuality(password) > PasswordManagerController.MINUM_SAFE_QUALITY);
+        utilityController.generatePassword();
+        String password = mainWindowAUI.getPasswordShown();
+
+        Assert.assertTrue(utilityController.checkQuality(password) > PasswordManagerController.MINUM_SAFE_QUALITY);
     }
 
     @Test
     public void exportImportFileTest() {
-    	//daten in das Modell eintragen
-    	File file = null; //FIXME: Dateipfad festlegen
-    	String masterPassword = "test";
-    	passwordManagerController.getPasswordManager().setMasterPassword(new BasePassword(masterPassword, 5, LocalDateTime.now()));
-    	Category root = passwordManagerController.getPasswordManager().getRootCategory();
-    	Category sub = new Category("sub");
-    	root.addSubCategory(sub);
-    	Category sub1 = new Category("sub1");
-    	sub.addSubCategory(sub1);
-    	Credentials c1 = new CredentialsBuilder("c1", "c2", "pw", "url")
-				.withChangeReminderDays(5)
-				.build();
-    	Credentials c2 = new CredentialsBuilder("c21", "c22", "pw2", "url2")
-				.withChangeReminderDays(6)
-				.build();
-    	sub.addCredentials(c1);
-    	sub.addCredentials(c2);
-    	sub1.addCredentials(c2);
-    	
-    	//Test
-    	utilityController.exportFile(file);
-    	utilityController.importFile(file, masterPassword);
-    	
-    	// Daten testen, ob diese im Modell vorhanden sind
-    	root = passwordManagerController.getPasswordManager().getRootCategory();
-    	Assert.assertFalse(root.getSubCategories().isEmpty());
-    	Assert.assertEquals(root.getSubCategories().size(), 1);
-    	
-    	Category subCategory1 = root.getSubCategories().stream().findFirst().get();
-    	Assert.assertEquals(subCategory1.getName(), "sub");
-    	Assert.assertFalse(subCategory1.getSubCategories().isEmpty());
-    	Assert.assertEquals(subCategory1.getSubCategories().size(), 1);
-    	
-    	Category subCategory2 = subCategory1.getSubCategories().stream().findFirst().get();
-    	Assert.assertEquals(subCategory2.getName(), "sub1");
-    	Assert.assertTrue(subCategory2.getSubCategories().isEmpty());
-    	
-    	Assert.assertFalse(subCategory1.getCredentials().isEmpty());
-    	Assert.assertEquals(subCategory1.getCredentials().size(), 2);
-    	Assert.assertFalse(subCategory2.getCredentials().isEmpty());
-    	Assert.assertEquals(subCategory2.getCredentials().size(), 1);
-    	
-    	Credentials credentials = subCategory2.getCredentials().stream().findFirst().get();
-    	Assert.assertEquals(credentials.getName(), c2.getName());
-    	Assert.assertEquals(credentials.getUserName(), c2.getUserName());
-    	Assert.assertEquals(credentials.getPassword(), c2.getPassword());
-    	Assert.assertEquals(credentials.getWebsite(), c2.getWebsite());
-    	Assert.assertEquals(credentials.getChangeReminderDays(), c2.getChangeReminderDays());
-    	Assert.assertEquals(credentials.getNotes(), c2.getNotes());
-    	   	
-    	//aufräumen
-    	file.delete();
+        //daten in das Modell eintragen
+        File file = null; //FIXME: Dateipfad festlegen
+        String masterPassword = "test";
+        passwordManagerController.getPasswordManager().setMasterPassword(masterPassword);
+        passwordManagerController.getPasswordManager().setMasterPasswordReminderDays(5);
+        passwordManagerController.getPasswordManager().setMasterPassordLastChanged(LocalDateTime.now());
+        Category root = passwordManagerController.getPasswordManager().getRootCategory();
+        Category sub = new Category("sub");
+        root.addSubCategory(sub);
+        Category sub1 = new Category("sub1");
+        sub.addSubCategory(sub1);
+        Credentials c1 = new CredentialsBuilder("c1", "c2", "pw", "url")
+                .withChangeReminderDays(5)
+                .build();
+        Credentials c2 = new CredentialsBuilder("c21", "c22", "pw2", "url2")
+                .withChangeReminderDays(6)
+                .build();
+        sub.addCredentials(c1);
+        sub.addCredentials(c2);
+        sub1.addCredentials(c2);
+
+        //Test
+        utilityController.exportFile(file);
+        utilityController.importFile(file, masterPassword);
+
+        // Daten testen, ob diese im Modell vorhanden sind
+        root = passwordManagerController.getPasswordManager().getRootCategory();
+        Assert.assertFalse(root.getSubCategories().isEmpty());
+        Assert.assertEquals(root.getSubCategories().size(), 1);
+
+        Category subCategory1 = root.getSubCategories().stream().findFirst().get();
+        Assert.assertEquals(subCategory1.getName(), "sub");
+        Assert.assertFalse(subCategory1.getSubCategories().isEmpty());
+        Assert.assertEquals(subCategory1.getSubCategories().size(), 1);
+
+        Category subCategory2 = subCategory1.getSubCategories().stream().findFirst().get();
+        Assert.assertEquals(subCategory2.getName(), "sub1");
+        Assert.assertTrue(subCategory2.getSubCategories().isEmpty());
+
+        Assert.assertFalse(subCategory1.getCredentials().isEmpty());
+        Assert.assertEquals(subCategory1.getCredentials().size(), 2);
+        Assert.assertFalse(subCategory2.getCredentials().isEmpty());
+        Assert.assertEquals(subCategory2.getCredentials().size(), 1);
+
+        Credentials credentials = subCategory2.getCredentials().stream().findFirst().get();
+        Assert.assertEquals(credentials.getName(), c2.getName());
+        Assert.assertEquals(credentials.getUserName(), c2.getUserName());
+        Assert.assertEquals(credentials.getPassword(), c2.getPassword());
+        Assert.assertEquals(credentials.getWebsite(), c2.getWebsite());
+        Assert.assertEquals(credentials.getChangeReminderDays(), c2.getChangeReminderDays());
+        Assert.assertEquals(credentials.getNotes(), c2.getNotes());
+
+        //aufräumen
+        file.delete();
     }
 
     @Test
     public void encryptDecryptTextTest() {
-    	String text = "password1";
-    	String encrypted = utilityController.encryptText(text);
-    	String decrypted = utilityController.decryptText(encrypted);
-    	Assert.assertEquals(text, decrypted);
+        String text = "password1";
+        EncryptedString encrypted = utilityController.encryptText(text);
+        String decrypted = utilityController.decryptText(encrypted);
+        Assert.assertEquals(text, decrypted);
+        Assert.assertNotEquals("encryption does not encrypt", text, encrypted.getEncryptedContent());
+        Assert.assertNotEquals("decryption does not decrypt", decrypted, encrypted.getEncryptedContent());
     }
 
 
     @Test
     public void checkQuality() {
-    	 //TODO:Braucht jetzt nicht.
+        //TODO:Braucht jetzt nicht.
 
     }
 }
