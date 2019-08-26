2aa0a0232b68b2d02ae3c8d40791085ccb8c7ba9
sopr040 <sopr040@fiws220.cs.tu-dortmund.de>
2019-08-26 14:33:25 +0200

    created methods SettingsViewController

--- src/de/sopra/passwordmanager/application/Main.java
+++ src/de/sopra/passwordmanager/application/Main.java
@@ -1,5 +1,8 @@
 package de.sopra.passwordmanager.application;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 import de.sopra.passwordmanager.controller.PasswordManagerController;
 import de.sopra.passwordmanager.view.LoginViewController;
 import de.sopra.passwordmanager.view.MainWindowViewController;
@@ -16,36 +19,48 @@ public class Main extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			/* Hauptfenster */
-			AnchorPane mainPane = new AnchorPane();
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Hauptfenster.fxml"));
-			mainPane = fxmlLoader.load();
-			mainWindowViewController = (MainWindowViewController) fxmlLoader.getController();
-			mainWindowViewController.setPasswordManagerController(new PasswordManagerController());
-			
-			mainStage = new Stage();
-			Scene mainScene = new Scene(mainPane);
-			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			mainStage.setScene(mainScene);
-			
-			/* Loginfenster */
-			AnchorPane loginPane = new AnchorPane();
-			fxmlLoader = new FXMLLoader(getClass().getResource("../view/Einloggen.fxml"));
-			loginPane = fxmlLoader.load();
-			LoginViewController loginViewController = (LoginViewController) fxmlLoader.getController();
-			loginViewController.setMainWindowViewController(mainWindowViewController);
-			loginViewController.setStage(primaryStage);
-			loginViewController.setMainStage(mainStage);
-			
-			
-			Scene loginScene = new Scene(loginPane);
-			loginScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(loginScene);
-			primaryStage.show();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		
+//		try {
+//			/* Hauptfenster */
+//			AnchorPane mainPane = new AnchorPane();
+//			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Hauptfenster.fxml"));
+//			mainPane = fxmlLoader.load();
+//			mainWindowViewController = (MainWindowViewController) fxmlLoader.getController();
+//			mainWindowViewController.setPasswordManagerController(new PasswordManagerController());
+//			
+//			mainStage = new Stage();
+//			Scene mainScene = new Scene(mainPane);
+//			mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			mainStage.setScene(mainScene);
+//			
+//			/* Loginfenster */
+//			AnchorPane loginPane = new AnchorPane();
+//			fxmlLoader = new FXMLLoader(getClass().getResource("../view/Einloggen.fxml"));
+//			loginPane = fxmlLoader.load();
+//			LoginViewController loginViewController = (LoginViewController) fxmlLoader.getController();
+//			loginViewController.setMainWindowViewController(mainWindowViewController);
+//			loginViewController.setStage(primaryStage);
+//			loginViewController.setMainStage(mainStage);
+//			
+//			
+//			Scene loginScene = new Scene(loginPane);
+//			loginScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(loginScene);
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
+		JDialog.setDefaultLookAndFeelDecorated(true);
+	    int response = JOptionPane.showConfirmDialog(null, "PasswortManager wirklich zurücksetzen? Dieser Vorgang löscht alle Daten endgültig!", "",
+	        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+	    if (response == JOptionPane.NO_OPTION) {
+	      System.out.println("");
+	    } else if (response == JOptionPane.YES_OPTION) {
+	      System.out.println("PasswortManager gelöscht.");
+	      mainWindowViewController.getPasswordManagerController().removeAll();
+	    } else if (response == JOptionPane.CLOSED_OPTION) {
+	      System.out.println("JOptionPane closed");
+	    }
 	}
 	
 	public static void main(String[] args) {
--- src/de/sopra/passwordmanager/view/SettingsViewController.java
+++ src/de/sopra/passwordmanager/view/SettingsViewController.java
@@ -1,25 +1,88 @@
 package de.sopra.passwordmanager.view;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import de.sopra.passwordmanager.controller.PasswordManagerController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
 public class SettingsViewController {
 
-    private MainWindowViewController mainWindowViewController;
+	private MainWindowViewController mainWindowViewController;
+	private Stage settingsStage, masterPasswordStage;
 
-    public void onChangeMasterpasswordClicked() {
+	public void onChangeMasterpasswordClicked() {
+		try {
+			/* MasterpasswortSetzenFenster */
+			AnchorPane setMasterPasswordPane = new AnchorPane();
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Masterpasswort-setzen.fxml"));
+			setMasterPasswordPane = fxmlLoader.load();
+			MasterPasswordViewController masterPasswordViewController = (MasterPasswordViewController) fxmlLoader
+					.getController();
 
+			masterPasswordStage = new Stage();
+			Scene setMasterPasswordScene = new Scene(setMasterPasswordPane);
+			setMasterPasswordScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			masterPasswordStage.setScene(setMasterPasswordScene);
+			masterPasswordStage.show();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public void onImportDataClicked() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Öffne Datei");
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("XML", "*.xml"));
+		File fileToOpen = fileChooser.showOpenDialog(null);
+		//TODO UtilityController -> import
+		//nutzt gleiche onLoginClicked() Methode wie Hauptlogin, muss entsprechend überschrieben werden
+		
+	}
+
+	public void onExportDataClicked() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Speichere Datei");
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("XML", "*.xml"));
+		File fileToSave = fileChooser.showSaveDialog(null);
+		mainWindowViewController.getPasswordManagerController().getUtilityController().exportFile(fileToSave);
+	}
+
+	public void onResetDataClicked() {
+		JDialog.setDefaultLookAndFeelDecorated(true);
+	    int response = JOptionPane.showConfirmDialog(null, "PasswortManager wirklich zurücksetzen? Dieser Vorgang löscht alle Daten endgültig!", "",
+	        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+	    if (response == JOptionPane.NO_OPTION) {
+	      //System.out.println("");
+	    } else if (response == JOptionPane.YES_OPTION) {
+	      System.out.println("PasswortManager gelöscht.");
+	      mainWindowViewController.getPasswordManagerController().removeAll();
+	    } else if (response == JOptionPane.CLOSED_OPTION) {
+	      //System.out.println("JOptionPane closed");
+	    }
+		
+	}
+
+	public void onCancelSettingsClicked() {
+		settingsStage.close();
     }
-
-    public void onImportDataClicked() {
-
-    }
-
-    public void onExportDataClicked() {
-
-    }
-
-    public void onResetDataClicked() {
-        //TODO: Sicherheitsabfrage
-        mainWindowViewController.getPasswordManagerController()
-                .removeAll();
-    }
+	
+	public void setStage(Stage settingsStage) {
+		this.settingsStage = settingsStage;
+	}
+	
+	
 
 }
