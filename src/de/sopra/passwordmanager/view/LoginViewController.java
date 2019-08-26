9c5800666ac57f8d754ebc88da98ef59dd9cff79
sopr043 <sopr043@fiws223.cs.tu-dortmund.de>
2019-08-26 09:56:23 +0200

    lvc comment

--- src/de/sopra/passwordmanager/view/LoginViewController.java
+++ src/de/sopra/passwordmanager/view/LoginViewController.java
@@ -1,16 +1,19 @@
 package de.sopra.passwordmanager.view;
 
-public class LoginViewController implements LoginViewAUI {
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class LoginViewController implements LoginViewAUI{
 
     private MainWindowViewController mainWindowViewController;
-
+    @FXML private PasswordField passwordField;
     public void onLoginClicked() {
-
+    	//mainWindowViewController.getPasswordManagerController().requestLogin(password, file);
     }
 
-
+    @Override
     public void handleLoginResult(boolean result) {
-
+    	
     }
 
 }
