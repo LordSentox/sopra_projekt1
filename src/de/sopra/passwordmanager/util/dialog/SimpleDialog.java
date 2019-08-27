29086c02e7e8f4c2daae769058a9210ae9385e5d
Juyas <juyasetgames@gmail.com>
2019-08-27 16:27:08 +0200

    Add SimpleDialog, dialog with no execution when hitting the ok button

---
+++ src/de/sopra/passwordmanager/util/dialog/SimpleDialog.java
@@ -0,0 +1,43 @@
+package de.sopra.passwordmanager.util.dialog;
+
+import javafx.scene.control.Alert;
+
+/**
+ * <h1>projekt1</h1>
+ *
+ * @author Julius Korweck
+ * @version 27.08.2019
+ * @since 27.08.2019
+ */
+public class SimpleDialog extends PasswordManagerDialog {
+
+    private String title, headerText, contentText;
+
+    public SimpleDialog(String title, String headerText, String text) {
+        this.title = title;
+        this.headerText = headerText;
+        this.contentText = text;
+    }
+
+    @Override
+    public void open() {
+        Alert alert = new Alert(alertType);
+        alert.initStyle(style);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+
+        alert.showAndWait();
+    }
+
+    @Override
+    public final void onCancel() {
+
+    }
+
+}
+/***********************************************************************************************
+ *
+ *                  All rights reserved, SpaceParrots UG (c) copyright 2019
+ *
+ ***********************************************************************************************/
