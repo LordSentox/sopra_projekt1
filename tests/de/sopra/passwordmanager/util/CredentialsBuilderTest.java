1e505b6e5ba8d457632a976049a33e906ab85a03
skadic <etienne.palanga@tu-dortmund.de>
2019-08-27 09:51:51 +0200

    Fix CredentialsBuilder test

--- tests/de/sopra/passwordmanager/util/CredentialsBuilderTest.java
+++ tests/de/sopra/passwordmanager/util/CredentialsBuilderTest.java
@@ -64,7 +64,7 @@ public class CredentialsBuilderTest {
         cred2.setChangeReminderDays(changeReminder);
         cred2.setLastChanged(lastChanged);
         cred2.setNotes(notes);
-        cred2.addSecurityQuestion(uc.encryptText(question1), uc.encryptText(question2));
+        cred2.addSecurityQuestion(uc.encryptText(question1), uc.encryptText(answer1));
         cred2.addSecurityQuestion(sq2);
 
         Assert.assertEquals("Credentials not equal", cred1, cred2);
@@ -111,7 +111,7 @@ public class CredentialsBuilderTest {
         Assert.assertEquals("Minimal built credentials not equal to expected", cred1, cred2);
     }
 
-    @Test(expected = CredentialsBuilder.CredentialsBuilderException.class)
+    @Test(expected = NullPointerException.class)
     public void missingNameTest() {
         String userName = "user1";
         String password = "passwort123";
@@ -125,7 +125,7 @@ public class CredentialsBuilderTest {
                 .build(uc);
     }
 
-    @Test(expected = CredentialsBuilder.CredentialsBuilderException.class)
+    @Test(expected = NullPointerException.class)
     public void missingUserNameTest() {
         String name = "cred1";
         String password = "passwort123";
@@ -140,7 +140,7 @@ public class CredentialsBuilderTest {
     }
 
 
-    @Test(expected = CredentialsBuilder.CredentialsBuilderException.class)
+    @Test(expected = NullPointerException.class)
     public void missingPasswordTest() {
         String name = "cred1";
         String userName = "user1";
@@ -154,7 +154,7 @@ public class CredentialsBuilderTest {
                 .build(uc);
     }
 
-    @Test(expected = CredentialsBuilder.CredentialsBuilderException.class)
+    @Test(expected = NullPointerException.class)
     public void missingWebsiteTest() {
         String name = "cred1";
         String userName = "user1";
@@ -168,7 +168,7 @@ public class CredentialsBuilderTest {
                 .build(uc);
     }
 
-    @Test(expected = CredentialsBuilder.CredentialsBuilderException.class)
+    @Test(expected = NullPointerException.class)
     public void invalidChangeReminderTest() {
         String name = "cred1";
         String userName = "user1";
